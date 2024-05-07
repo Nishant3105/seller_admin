8eedf81a2ca4c63213d57e958efa654832c94547
Nishant3105 <nishantkumar2522@gmail.com>
2024-05-07 14:37:43 +0530

    Delete_functionality

--- src/Components/UI.js
+++ src/Components/UI.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './Card'
 
 const UI = (props) => {
-
+    
     let foodData=props.allFormData.filter((formData) => {
         localStorage.setItem(formData.productId.toString(),JSON.stringify(formData))
         return formData.category === "food"
@@ -15,6 +15,21 @@ const UI = (props) => {
         localStorage.setItem(formData.productId.toString(),JSON.stringify(formData))
         return formData.category === "skincare"
     })
+
+    const deleteHandler = (id,cat) => {
+        localStorage.removeItem(id.toString())
+        let parent
+        switch (cat){
+            case 'food':  parent=document.getElementById('food')
+            break
+            case 'electronics':  parent=document.getElementById('electronics')
+            break
+            case 'skincare':  parent=document.getElementById('skincare')
+            break
+        }
+        let childToRemove = document.getElementById(id);
+        parent.removeChild(childToRemove);
+    }
     
     foodData= foodData.length===0 ? [] : foodData
     electronicsData= electronicsData.length===0 ? [] : electronicsData
@@ -24,24 +39,27 @@ const UI = (props) => {
         <Card>
             <ul id="food">Food
                 {foodData.map((formData) => (
-                    <li key={formData.productId}>
+                    <li id={formData.productId}>
                         {formData.sellingPrice} {formData.productName} {formData.category}
+                        <button onClick={() => deleteHandler(formData.productId, formData.category)}>Delete</button>
                     </li>
                 ))
                 }
             </ul>
             <ul id="electronics">Electronics
                 {electronicsData.map((formData) => (
-                    <li key={formData.productId}>
+                    <li id={formData.productId}>
                         {formData.sellingPrice} {formData.productName} {formData.category}
+                        <button onClick={() => deleteHandler(formData.productId, formData.category)}>Delete</button>
                     </li>
                 ))
                 }
             </ul>
             <ul id="skincare">Skincare
                 {skincareData.map((formData) => (
-                    <li key={formData.productId}>
+                    <li id={formData.productId}>
                         {formData.sellingPrice} {formData.productName} {formData.category}
+                        <button onClick={() => deleteHandler(formData.productId, formData.category)}>Delete</button>
                     </li>
                 ))
                 }
